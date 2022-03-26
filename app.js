19378849f9849ca54920a4f8038fdc8ece3f74b1
Ivanildo Paiva <91390007+IvanildoPaiva@users.noreply.github.com>
2022-03-26 09:52:40 -0400

    Projeto finalizado, os dados vem do local storage

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
+//Criar o banco de Dados para armazenar as tarefas
+// let banco = [
+//     { 'tarefa': 'Estudar React-Native', 'status':''},
+//     { 'tarefa': 'Estudar JS', 'status':'checked'},
+//     { 'tarefa': 'Estudar HTML5', 'status':'checked'},
+// ];
 
-// Criar o banco de Dados para armazenar as tarefas
-let banco = [
-    { 'tarefa': 'Estudar React-Native', 'status':''},
-    { 'tarefa': 'Estudar JS', 'status':'checked'},
-    { 'tarefa': 'Estudar HTML5', 'status':'checked'},
-];
+const getBanco = () =>JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco));
 
    const criarItem = (tarefa, status, indice) => {
    const item = document.createElement('label'); 
@@ -25,30 +27,43 @@ const limpaTarefas = () =>{
     }
 }
 const atualizaTela = () => {
-    limpaTarefas();
-    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+        limpaTarefas();
+        const banco = getBanco();
+       banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+      
 }
+
+// saber qual tecla digitou no campo input
+
 const inserirItem = (evento) =>{
     const tecla = evento.key;
     const texto = evento.target.value;
     if(tecla === 'Enter'){
+        //depois de realizar a comparação da tecla Enter, faz uma inserção da tarefa
+        const banco = getBanco();
         banco.push({'tarefa': texto, 'status':''});
+        setBanco(banco);
         atualizaTela();
         evento.target.value = '';
     }
 }   
 const removerItem = (indice) =>{
+    const banco = getBanco();
     banco.splice(indice, 1);
+    setBanco(banco);
     atualizaTela();
 }
 
 const atualizarItem = (indice) =>{
-    banco[indice].status =  banco[indice].status === '' ? 'checked':'';
-    atualizaTela();
+    const banco = getBanco();
+        banco[indice].status =  banco[indice].status === '' ? 'checked':'';
+        setBanco(banco);
+        atualizaTela();
+
 }
 const clickItem = (evento) => {
     const elemento = evento.target;
-    if(elemento.type==='button'){
+    if(elemento.type ==='button'){
         const indice = elemento.dataset.indice; 
         removerItem(indice);
     }else if (elemento.type ==='checkbox'){
